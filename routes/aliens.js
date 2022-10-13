5333d6f52450fcaeb0c3fff0565b050facf6a0eb
Lourenço Lima <51246899+LourencoDev@users.noreply.github.com>
2022-10-13 20:51:51 -0300

    Update aliens.js

--- routes/aliens.js
+++ routes/aliens.js
@@ -7,7 +7,7 @@ router.get('/', async(req,res) => {
         const aliens = await Alien.find();
         res.json(aliens);
     }catch(err){
-        res.send('Error '+err);
+        res.status(500).json({ err });
     }
 })
 
@@ -16,45 +16,40 @@ router.get('/:id', async(req,res) => {
         const alien = await Alien.findById(req.params.id);
         res.json(alien);
     }catch(err){
-        res.send('Error '+err);
+        res.status(500).json({ err });
     }
 })
 
 router.post('/', async(req,res) => {
-    const alien = new Alien({
-        name: req.body.name,
-        tech: req.body.tech,
-        sub: req.body.sub
-    })
+    const { name, tech, sub } = req.body;
+    const alien = new Alien({ name, tech, sub });
 
     try{
         const a1 = await alien.save();
         res.send(a1);
     }catch(err){
-        res.send('Error '+err);
+        res.status(500).json({ err });
     }
 })
 
 router.patch('/:id', async(req,res) => {
     try{
         const alien = await Alien.findById(req.params.id);
-        if (req.body.name===undefined) {
-        } else {
-            alien.name = req.body.name;
+        const { name, tech, sub } = req.body;
+        if (!name) {
+            alien.name = name;
         }
-        if (req.body.tech===undefined) {
-        } else {
-            alien.tech = req.body.tech;
+        if (!tech) {
+            alien.tech = tech;
         }
-        if (req.body.sub===undefined) {
-        } else {
-            alien.sub = req.body.sub;
+        if (!sub) {
+            alien.sub = sub;
         }
         
         const a1 = await alien.save();
         res.json(a1);
-    }catch(err){
-        res.send('Error '+err)
+    }catch (err) {
+        res.status(500).json({ err });
     }
 })
 
@@ -63,8 +58,8 @@ router.delete('/:id', async(req,res) => {
         const alien = await Alien.findById(req.params.id);
         const a1 = await alien.deleteOne();
         res.json(a1);
-    }catch(err){
-        res.send('Error '+err);
+    } catch(err) {
+        res.status(500).json({ err });
     }
 })
 
